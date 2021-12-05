15e5fe933ab5061c6935c6b7152fbc0e8302c544
Linushf <linus.furubom@gmail.com>
2021-12-05 02:00:32 +0000

    Review all js-code add reset function and increment score function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,44 @@
-const restart = document.getElementById('reset');
-const choices = document.querySelectorAll('choices');
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-let user_score = document.getElementById('user-score');
-let userScore = 0;
+// Players choice
+
+document.addEventListener("DOMContentLoaded", function() {
+    let rock = document.getElementById('rock');
+    let paper = document.getElementById('paper');
+    let scissors = document.getElementById('scissors');
+
+
+    rock.addEventListener('click', function() {
+    playGame('rock');
+    })
+    paper.addEventListener('click', function() {
+    playGame('paper');
+    })
+    scissors.addEventListener('click', function() {
+    playGame('scissors');
+})
+})
+
+// Game play
+
+function playGame(userChoice) {
+
+    let computerPick = getComputerChoice();
+    resetButton();
+    if (computerPick === userChoice) {
+        console.log('Its a draw');
+    } else if (computerPick === 'paper' && userChoice === 'scissors', 
+        computerPick === 'scissors' && userChoice === 'rock',
+        computerPick === 'rock' && userChoice === 'paper') {
+        console.log('user win');
+        incrementUserScore();
+    } else if (computerPick === 'rock' && userChoice === 'scissors',
+        computerPick === 'scissors' && userChoice === 'paper',
+        computerPick === 'paper' && userChoice === 'rock') {
+        console.log('Mysterion win');
+        incrementComputerScore();
+    }
+}
+
+// Calculate a random choice from computer
 
 function getComputerChoice() {
     let pick = ['rock', 'paper', 'scissors'];
@@ -12,28 +46,32 @@ function getComputerChoice() {
     return pick [randomNumber];
 }
 
-function game(userChoice) {
-    restart.style.display = 'block';
-    let computerPick = getComputerChoice();
+// Incrementing the scores
+
+function incrementUserScore() {
+    let oldUserScore = (document.getElementById("uscore").innerText);
+    document.getElementById("uscore").innerText = ++oldUserScore;
+}
+
+function incrementComputerScore() {
+    let oldComputerScore = (document.getElementById("mscore").innerText);
+    document.getElementById("mscore").innerText = ++oldComputerScore;
+}
+
+// Reset score
+function resetButton() {
+    let reset = document.getElementById('reset');
+    reset.style.display = 'block';
     
-    if (computerPick === userChoice) {
-        console.log('draw');
-    } else if (computerPick === 'paper' && userChoice === 'scissors') {
-        console.log('user win');
-    } 
+    reset.addEventListener('click', function() {
+    let ok = confirm('Do you really want to reset the game?');
+        if (ok == true) {
+        document.getElementById("uscore").innerText = 0;
+        document.getElementById("mscore").innerText = 0;
+      } else {
+        alert(`Let's continue!`);
+      }
+    })
 }
 
-function main() {
-    rock.addEventListener('click', function() {
-    game('rock');
-})
-    paper.addEventListener('click', function() {
-    game('paper');
-})
-    scissors.addEventListener('click', function() {
-    game('scissors');
-})
-
-}
-
-main();
+// Pop-ups
